91d8cf4162afbdd53cc394659b82deb43850be78
Blackout1403 <brenainroberto@gmail.com>
2025-11-06 16:57:04 +0000

    Endpoints ejem

--- api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
+++ api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import mx.tecnm.backend.api.models.Producto;
+
 
 @RequestMapping("/test")
 @RestController
@@ -12,5 +14,27 @@ public class Test {
     @GetMapping("/hello")
     public String helloworld(){
         return "Hola API rest";
+    }
+
+    @GetMapping("/producto")
+    public Producto getProducto(){
+        Producto p = new Producto();
+        p.nombre = "Coca Cola";
+        p.precio = 17.2;
+        p.codigoBarras= "7501053123606";
+        return p;
+    }
+
+
+    @GetMapping
+    public Producto[] getProductos(){
+        Producto p1 = new Producto();
+        p1.nombre = "Coca Cola";
+        p1.precio = 18.5 ;
+        p1.codigoBarras = "751927392383";
+
+
+
+        return new Producto[]{p1}
     }
 }
---
+++ api/src/main/java/mx/tecnm/backend/api/models/Producto.java
@@ -0,0 +1,8 @@
+package mx.tecnm.backend.api.models;
+
+public class Producto {
+public String nombre;
+public Double precio;
+public String codigoBarras;
+
+}
