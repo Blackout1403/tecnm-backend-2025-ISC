eeaaed50075610b21caff067465dbd127e0378ec
Blackout1403 <brenainroberto@gmail.com>
2025-11-06 17:02:48 +0000

    Ejemplo completo

--- api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
+++ api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
@@ -26,15 +26,23 @@ public class Test {
     }
 
 
-    @GetMapping
+    @GetMapping("/productos")
     public Producto[] getProductos(){
         Producto p1 = new Producto();
         p1.nombre = "Coca Cola";
         p1.precio = 18.5 ;
         p1.codigoBarras = "751927392383";
 
+              Producto p2 = new Producto();
+        p2.nombre = "Coca Cola";
+        p2.precio = 18.5 ;
+        p2.codigoBarras = "751927392383";
 
+              Producto p3 = new Producto();
+        p3.nombre = "Coca Cola";
+        p3.precio = 18.5 ;
+        p3.codigoBarras = "751927392383";
 
-        return new Producto[]{p1}
+        return new Producto[]{p1,p3,p2};
     }
 }
