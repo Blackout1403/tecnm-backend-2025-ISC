6a010e3b1f43bfbf7b8134a7013c70d9940636b9
Blackout1403 <brenainroberto@gmail.com>
2025-11-25 17:03:27 +0000

    Cambios Antes de

--- api/src/main/java/mx/tecnm/backend/api/controllers/CategoriaController.java
+++ api/src/main/java/mx/tecnm/backend/api/controllers/CategoriaController.java
@@ -1,12 +1,14 @@
 package mx.tecnm.backend.api.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
-import mx.tecnm.backend.api.models.Categoria;
+
+import mx.tecnm.backend.api.models.Categoria.categoria;
 import mx.tecnm.backend.api.repository.CategoriaDAO;
 
 @RestController
@@ -16,9 +18,10 @@ public class CategoriaController {
     @Autowired
     CategoriaDAO repo;
 
-
-    public ResponseEntity<List<Categoria>> obtenerCategorias() {
-    
+    @GetMapping()
+    public ResponseEntity<List<categoria>> obtenerCategorias() {
+        List<categoria> categorias = repo.consultarCategorias();
+        return ResponseEntity.ok(categorias);
     } 
 
 }
