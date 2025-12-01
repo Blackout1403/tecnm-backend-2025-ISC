c70d1ff643d2da9ba9fb31af21effa30ff8d8e26
LloniElias01 <juancarloseliasnava@gmail.com>
2025-12-01 16:43:27 +0000

    Endpoints

--- api/src/main/java/mx/tecnm/backend/api/controllers/UsuarioController.java
+++ api/src/main/java/mx/tecnm/backend/api/controllers/UsuarioController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,16 @@ public class UsuarioController {
         List<Usuario> usuarios = repo.consultarUsuario();
         return ResponseEntity.ok(usuarios);
     } 
+
+    @PostMapping()
+    public ResponseEntity<Usuario> agregarUsuario(Usuario usuario) {
+        Usuario nuevoUsuario = repo.agregarUsuario(usuario);
+        return ResponseEntity.ok(nuevoUsuario);
+    }
+
+    @PutMapping()
+    public ResponseEntity<Usuario> actualizarUsuario(Usuario usuario) {
+        usuario = repo.agregarUsuario(usuario);   
+        return ResponseEntity.ok(usuario);
+    }
 }
--- api/src/main/java/mx/tecnm/backend/api/models/Usuario.java
+++ api/src/main/java/mx/tecnm/backend/api/models/Usuario.java
@@ -1,6 +1,8 @@
 package mx.tecnm.backend.api.models;
 
+import java.sql.Date;
 
- public record Usuario(int id, String nombre) {
+public record Usuario(int id, String nombre, String email, String telefono, String sexo, Date Fecha_nacimineto, String contrasena, Date fecha_registro) {
     }
+
 
